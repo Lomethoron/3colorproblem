56c130cc603ef731af16a24b2f17a7764389b473
Will Dixon <wdixon23192@gmail.com>
2015-07-22 12:21:35 -0400

    Fixed stuff

--- ThreeColorProblem.java
+++ ThreeColorProblem.java
@@ -1,24 +1,32 @@
 import java.util.*;
+import java.io.*;
 public class ThreeColorProblem{
 	public static void main(String args[]){
-		Scanner in = new Scanner(new File(args[0]));
-		
-		String setupInfo = in.nextLine();
-		Hashmap<Integer, Vertex> graph = new Hashmap<Integer,Vertex>(setupInfo.split()[1]);
-		while(in.hasNextLine()){
-			String newEdge = in.nextLine();
-			String[] newEdgeSplit = newEdge.split();
-			if(!graph.contains(newEdgeSplit[0])){
-				Vertex newVertex = new Vertex();
-				graph.put(newEdgeSplit[0],newVertex);
+		try{
+			Scanner in = new Scanner(new File(args[0]));
+			String setupInfo = in.nextLine();
+			String[] setupInfoSplit = setupInfo.split(" ");
+			HashMap<String, Vertex> graph = new HashMap<String,Vertex>();
+			while(in.hasNextLine()){
+				String newEdge = in.nextLine();
+				String[] newEdgeSplit = newEdge.split(" ");
+				if(!graph.containsKey(newEdgeSplit[0])){
+					Vertex newVertex = new Vertex();
+					graph.put(newEdgeSplit[0],newVertex);
+				}
+				if(!graph.containsKey(newEdgeSplit[1])){
+					Vertex newVertex = new Vertex();
+					graph.put(newEdgeSplit[0],newVertex);
+				}
+				Vertex vertex1 = graph.get(newEdgeSplit[0]);
+				vertex1.giveEdge(newEdgeSplit[1]);
+				Vertex vertex2 = graph.get(newEdgeSplit[1]);
+				vertex2.giveEdge(newEdgeSplit[0]);
 			}
-			if(!graph.contains(newEdgeSplit[1])){
-				graph.put(newEdgeSplit[0],newVertex);
-			}
-			Vertex vertex1 = graph.get(newEdgeSplit[0]);
-			vertex1.giveEdge(newEdgeSplit[1]);
-			Vertex vertex2 = graph.get(newEdgeSplit[1]);
-			vertex2.giveEdge(newEdgeSplit[0]);
+			in.close();
+		}
+		catch(Exception e) {
+			System.out.println(e);
 		}
 	}
 }
